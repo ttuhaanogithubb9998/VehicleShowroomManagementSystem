9f48d978ff560954c809e8591dd1763a6f552705
TuanzNguyez <you@example.com>
2022-03-19 10:03:43 +0700

    add controller checkadmin

--- VehicleShowroomManagementSystem/Areas/Admin/Controllers/CheckAdminController.cs
+++ VehicleShowroomManagementSystem/Areas/Admin/Controllers/CheckAdminController.cs
@@ -1,13 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VehicleShowroomManagementSystem.Data;
+using VehicleShowroomManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+
 
 namespace VehicleShowroomManagementSystem.Areas.Admin.Controllers
+
 {
+    [Area("Admin")]
     public class CheckAdminController : Controller
     {
+
+
+        public readonly VehicleShowroomManagementSystemContext _context;
+
+        public CheckAdminController(VehicleShowroomManagementSystemContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public Employee CheckAdmin()
+        {
+            string admin = HttpContext.Session.GetString("Account");
+
+            var employee = _context.Employees.FirstOrDefault(e => e.Account == admin && e.Position == "Admin");
+            ViewBag.employee = employee;
+
+            return employee;
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
